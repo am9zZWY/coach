bff7746d43c6095e57d8cf5479f4f23a3f023538
Josef Müller <46693545+am9zZWY@users.noreply.github.com>
2025-06-05 09:16:21 +0200

    feat(coach)!: disable Mail view for now

--- coach/src/views/HomeView.vue
+++ coach/src/views/HomeView.vue
@@ -1,36 +1,31 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import { Separator } from '@/components/ui/separator'
 import Tasks from '@/components/task/Tasks.vue'
-import { useTaskStore } from '@/stores/task.ts'
 import Summary from '@/components/Summary.vue'
 import CookingSuggestion from '@/components/CookingSuggestion.vue'
 import MailList from '@/components/mail/MailList.vue'
 import { Card } from '@/components/ui/card'
-
-const taskStore = useTaskStore()
-const taskCount = computed(() => taskStore.flatTasks.length)
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <!-- Main Content Area -->
-    <div class="flex flex-col-reverse lg:flex-row gap-4">
-      <!-- Left Column: Summary & Tasks (stacked vertically) -->
-      <Summary class="lg:w-2/5" />
-      <Tasks class="lg:w-3/5" />
+    <div class="container mx-auto">
+        <!-- Main Content Area -->
+        <div class="flex flex-col-reverse lg:flex-row gap-4">
+            <!-- Left Column: Summary & Tasks (stacked vertically) -->
+            <Summary class="lg:w-2/5"/>
+            <Tasks class="lg:w-3/5"/>
 
-      <!-- Right Column: Calendar -->
-      <Card class="lg:w-1/2">
-        <MailList />
-      </Card>
+            <!-- Right Column: Calendar -->
+            <Card class="lg:w-1/2" v-if="false">
+                <MailList/>
+            </Card>
+        </div>
+
+        <Separator orientation="horizontal" class="my-8 opacity-70"/>
+
+        <!-- Cooking Suggestion Section -->
+        <section>
+            <CookingSuggestion/>
+        </section>
     </div>
-
-    <Separator orientation="horizontal" class="my-8 opacity-70" />
-
-    <!-- Cooking Suggestion Section -->
-    <section>
-      <CookingSuggestion />
-    </section>
-  </div>
 </template>
